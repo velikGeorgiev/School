2234e38b1b843d3d06793b080663ba5df35532f5
velkoSimarro <velko_bg@abv.bg>
2014-10-30 22:38:17 +0100

    Edit

--- PRG/Examen 1/Ejercicio3_VelikGeorgiev.java
+++ PRG/Examen 1/Ejercicio3_VelikGeorgiev.java
@@ -1,90 +1,39 @@
-package Examen_Tema3_VelikGeorgiev;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyTests;
 
 import java.util.Scanner;
 
 /**
  *
- * @author Velik Georgiev Chelebiev
+ * @author velko
  */
-public class Ejercicio3_VelikGeorgiev {
-
+public class Actividad3 {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        /**
-         * El numero total de entradas introducidas por el usuario.
-         */
-        int totalEntradas = 0;
         
-        /**
-         * Entrada actual del usuario.
-         */
-        int entradaUsuario = 0;
-        
-        /**
-         * Un vector que guardara temporalmente las dos ultimas entradas
-         * introducidas por el usuario.
-         */
-        int[] entradasTemporales = new int[2];
-        
-        /**
-         * La resta de las dos ultimas entradas.
-         */
-        int resta = 0; 
+        int temp1 = 0, temp2 = 0, input = 0, resta = 0, entradas = 0;
         
         do {
-            try {
-                entradaUsuario = Integer.parseInt(scan.nextLine());
-            } catch(Exception ex) {
-                System.out.println("*** Valor no valido ***");
-                continue;
-            }
+            temp1 = temp2;
+            temp2 = input;
             
-            /**
-             * Calculamos la resta de los dos numeros anteriores
-             */
-            if(totalEntradas >= 1) {
-                if(totalEntradas % 2 == 1){
-                    resta = entradasTemporales[1] - entradasTemporales[0];
-                }else {
-                    resta = entradasTemporales[0] - entradasTemporales[1];
-                }
-            }
+            input = Integer.parseInt(scan.nextLine());
             
-            /**
-             * Si la cantidad de entradas en mayor a 2 ( es decir ya tenemos una resta )
-             * comprobamos si la resta de los dos numeros anteriores es menor que el
-             * numero introducido
-             */
-            if(totalEntradas >= 2) {
-                if(entradaUsuario < resta) {
-                    break;
-                }
-            }
+            resta = temp1 - temp2;
             
-            /**
-             * Asignamos el numero a una variable temporal
-             */
-            if(totalEntradas % 2 == 0){
-                entradasTemporales[0] = entradaUsuario;
-            } else {
-                entradasTemporales[1] = entradaUsuario;
-            }
-            
-            // Aumentamos las entradas
-            totalEntradas++;
-        }while(true);
+            entradas++;
+        }while(resta < input);
         
-        /**
-         * Imprimimos la salida del programa.
-         */
-        System.out.println("\n****************\n");
-        System.out.println("Total entradas: " + (totalEntradas + 1));
-        System.out.println("Valores que no cumplen la condicion: ");
-        System.out.println("Valor 1: " + entradasTemporales[0]);
-        System.out.println("Valor 2: " + entradasTemporales[1]);
-        System.out.println("Valor 3: " + entradaUsuario);
+        System.out.println("Total entradas: " + entradas + "\nValores NO validos:");
+        System.out.println("Valor 1: " + temp1);
+        System.out.println("Valor 2: " + temp2);
+        System.out.println("Valor 3: " + input);
     }
 }
